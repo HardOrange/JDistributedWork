a7f001c9c96459235af72c796b2788f50e75fb59
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-19 10:19:22 -0500

    Threaded Connections in WorkServer
    Added Socket Connections to be Threaded, so that Clients can connect and
    disconnect at will, and execution of Work distribution can still
    continue in WorkServer.

--- Server/WorkServer.java
+++ Server/WorkServer.java
@@ -2,22 +2,48 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class WorkServer{
+		ArrayList<ClientConnection> Sessions; //ArrayList Containing all the Active Client Connections
 	public WorkServer(String[] args){
-		try{
-			ArrayList<ClientConnection> sessions = new ArrayList<ClientConnection>();
-			ServerSocket servSocket = new ServerSocket(3308);
-			int currentSessionCount = 0;
-			for(;;currentSessionCount++){
-				Socket currSocket = servSocket.accept();
-				ClientConnection currConn = new ClientConnection(currSocket, currentSessionCount);
-				currConn.start();
-				sessions.add(currConn);
-			}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+		Sessions = new ArrayList<ClientConnection>();
+		new WorkServerConnectionThread().start();
 	}
+
+	public void newConnection(ClientConnection currConn){
+		Sessions.add(currConn);
+	}
+	public void removeConnection(ClientConnection currConn){
+		Sessions.remove(currConn);
+	}
+
 	public static void main(String[] args){
 		new WorkServer(args);
 	}
+}
+
+
+public class WorkServerConnectionThread extends Thread{
+	private boolean stillAcceptingConnections=true;
+	private WorkServer Superior;
+	public WorkServerConnectionThread(WorkServer superior){
+		Superior = superior;
+	}
+	public void run(){
+		ServerSocket servSocket = new ServerSocket(3308);
+		int currentSessionCount = 0;
+		servSocket.setSoTimeout(200);
+		while(stillAcceptingConnections){
+			try{
+				Socket currSocket = servSocket.accept();
+				currentSessionCount++;
+				ClientConnection currConn = new ClientConnection(currSocket, currentSessionCount);
+				Superior.newConnection(currConn);
+				currConn.start();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		servSocket.close();
+	}
+
 }
