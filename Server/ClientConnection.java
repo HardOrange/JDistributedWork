967299df6a6ab131b8cdc649fe9741ace246cb2f
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-19 11:25:26 -0500

    Updated ClientConnection
    added the getSocket Method.

--- Server/ClientConnection.java
+++ Server/ClientConnection.java
@@ -1,7 +1,12 @@
 import java.net.*;
 public class ClientConnection extends Thread{
+	private Socket ClientSocket;
 	public ClientConnection(Socket clientSocket, int connectionNumber){
+		
+	}
 
+	public Socket getSocket(){
+		return ClientSocket;
 	}
 
 }
