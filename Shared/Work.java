23dec187397ab3742dd3c8cddb0fc333af03aadc
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-19 10:00:07 -0500

    Commented Work

--- Shared/Work.java
+++ Shared/Work.java
@@ -1,7 +1,7 @@
 public class Work{
-	private int Status=0;
+	private int Status=0; //Status indicator, 0 meaning no action, 1 meaning currently being worked on, and 2 meaning result is gathered;
 	private String Result;
-	private Runnable Target;
+	private Runnable Target; //The Runnable class that the work will be designed in.
 	public Work(Runnable target){
 		Target= target;
 	}
