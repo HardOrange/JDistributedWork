0a53907b21e36e075c1de1a6a049eaa14f2ab1bf
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-19 09:58:55 -0500

    Updated Work.java
    Added Global Variables and the Get and Set Methods.

--- Shared/Work.java
+++ Shared/Work.java
@@ -1,5 +1,22 @@
 public class Work{
+	private int Status=0;
+	private String Result;
+	private Runnable Target;
 	public Work(Runnable target){
+		Target= target;
+	}
 
+	public void setStatus(int status){
+		Status = status;
+	}
+
+	public String getResult(){
+		return Result;
+	}
+	public int getStatus(){
+		return Status;
+	}
+	public Runnable getRunnable(){
+		return Target;
 	}
 }
