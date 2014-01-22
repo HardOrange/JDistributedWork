9240374e8125559a3cdcf9b1c200019759bfd1f3
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-22 16:02:32 -0500

    Added ReportThread
    This Abstract Class should replace Work, and should be sent over the
    Sockets instead of Threads

---
+++ ReportThread.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public abstract class ReportThread extends Thread implements Serializable{
+	int Status;
+	String Result;
+	int SliceNum;
+
+	int getStatus(){
+		return Status;
+	}
+
+	void setStatus(int status){
+		Status = status;
+	}
+
+	String getResult(){
+		return Result;
+	}
+
+	final int getSliceNum(){
+		return SliceNum;
+	}
+
+
+
+}
